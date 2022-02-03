1a3165c0cbc4cb072dd947d1a07ceb5355627e5d
sanchitdawarsd <sanchit1045.cse18@chitkara.edu.in>
2022-02-03 12:43:47 +0530

    correct config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ const chalk = require("chalk");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 
-const defaultNetwork = "bsctestnet"; // "hardhat" for tests
+const defaultNetwork = "localhost"; // "hardhat" for tests
 const API = process.env.NODE_API;
 const PRIVATE_KEY = process.env.PRIVATEKEY;
 const PRIVATE_KEY2 = process.env.PRIVATEKEY2;
@@ -15,7 +15,7 @@ module.exports = {
   defaultNetwork,
   networks: {
     localhost: {
-      url: `http://localhost:8545/`,
+      url: `https://data-seed-prebsc-1-s2.binance.org:8545/`,
       accounts: [`0x${PRIVATE_KEY}`,`0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`],
     },
     mainnet: {
